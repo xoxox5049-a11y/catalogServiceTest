037ba6efdc25dec748195d294a632dffddcd9064
testXo1 <xoxox5049@gmail.com>
2025-08-30 12:19:29 +0300

    fix bag with sku

--- src/main/java/com/catalogservice/dto/ProductCreateRequestDto.java
+++ src/main/java/com/catalogservice/dto/ProductCreateRequestDto.java
@@ -2,7 +2,11 @@ package com.catalogservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ProductCreateRequestDto extends BaseDto{
     @NotBlank
     @Size(min=3, max=32)
